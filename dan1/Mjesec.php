4fea1853eb4a0386e40d121ecf10c0837dfc9afe
NinaSvec <55713067+NinaSvec@users.noreply.github.com>
2019-10-14 21:20:01 +0200

    Add files via upload

---
+++ dan1/Mjesec.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'Datum.php';
+
+class Mjesec extends Datum{
+
+    private $mjeseci = array('Siječanj','Veljača','Ožujak');
+
+    public function getMonthName($date){
+        $mjesec = date('n',strtotime($date));
+        $mjesec--;
+        $ime_mjeseca = $this->mjeseci[$mjesec];
+        return "Danas je dan " . $this->getDayName($date) . " u mjesecu $ime_mjeseca";
+
+    }
+
+}
+
+$mjesec = new Mjesec ();
+echo $mjesec->getMonthName('2019-02-15');
